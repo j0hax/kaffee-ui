71d7cd9ef27fc8fce967bc9698adc84448e9d834
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-03-12 16:50:59 +0100

    Compartmentalized function

--- js/table.js
+++ js/table.js
@@ -1,23 +1,27 @@
-const table = document.getElementById('table')
-const users = get_users()
+function updateTable() {
+    const table = document.getElementById('table')
+    const users = get_users()
 
-users.forEach(function (item, index, array) {
-  row = table.insertRow()
-  nameCell = row.insertCell()
-  countCell = row.insertCell()
-  balanceCell = row.insertCell()
-  lastChange = row.insertCell()
+    users.forEach(function (item, index, array) {
+    row = table.insertRow()
+    nameCell = row.insertCell()
+    countCell = row.insertCell()
+    balanceCell = row.insertCell()
+    lastChange = row.insertCell()
 
-  const euro = (item.balance - item.drinkCount * drinkPrice) / 100
-  const lastUpdate = new Date(item.lastUpdate)
+    const euro = (item.balance - item.drinkCount * drinkPrice) / 100
+    const lastUpdate = new Date(item.lastUpdate)
 
-  nameCell.innerHTML = item.name
-  countCell.innerHTML = item.drinkCount
-  balanceCell.innerHTML = euro.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
+    nameCell.innerHTML = item.name
+    countCell.innerHTML = item.drinkCount
+    balanceCell.innerHTML = euro.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })
 
-  if (euro <= -10) {
-    balanceCell.classList.add('debt')
-  }
+    if (euro <= -10) {
+        balanceCell.classList.add('debt')
+    }
 
-  lastChange.innerHTML = lastUpdate.toLocaleString('de-DE')
-})
+    lastChange.innerHTML = lastUpdate.toLocaleString('de-DE')
+    })
+}
+
+window.onload = updateTable();
