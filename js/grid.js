d1e4587aab6f00b985b72da05ff3e20d150bab72
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-05-18 16:40:24 +0200

    Optimize Javascript execution

--- js/grid.js
+++ js/grid.js
@@ -1,15 +1,20 @@
-function updateGrid(users) {
+
+/**
+ * Updates the Grid with a an array of users
+ * @param {Array} users
+ */
+async function updateGrid(users) {
   grid = document.getElementById('user-grid')
 
   // clear
   grid.innerHTML = ''
 
-  console.log("Adding", users.length, "users...")
+  console.log('Adding', users.length, 'users...')
 
   for (i = 0; i < users.length; i++) {
     // our column
     const column = document.createElement('div')
-    column.className = "col"
+    column.className = 'col'
 
     // our card
     const card = document.createElement('div')
@@ -28,27 +33,22 @@ function updateGrid(users) {
     body.appendChild(title)
 
     // add text
-    const balance = Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(users[i].balance / 100);
+    const balance = Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(users[i].balance / 100)
     const text = document.createElement('p')
-    text.className = "card-text"
-    text.innerHTML = "Saldo: " + balance
+    text.className = 'card-text'
+    text.innerHTML = 'Saldo: ' + balance
+
+    if (users[i].balance < -1000) {
+      text.classList.add('debt')
+    }
+
     body.appendChild(text)
 
     // Process clicks
-    card.onclick = async function () {
-      add_drink(title.innerHTML).then(get_users().then((users) => updateGrid(users)))
+    card.onclick = function () {
+      add_drink(title.innerHTML)
     }
 
     grid.appendChild(column)
   }
-}
-
-window.onload = function () {
-  console.log("Document loaded, creating user grid...")
-  get_users().then((users) => updateGrid(users))
-
-  /*setInterval(function () {
-    console.log("[Autorefresh]")
-    updateGrid()
-  }, 30000)*/
 }
