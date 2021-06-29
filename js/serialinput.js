ec0dce9b722b18e5617be8f8aaba1f09e84d578a
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-06-29 13:00:06 +0200

    Refactoring

--- js/serialinput.js
+++ js/serialinput.js
@@ -1,25 +1,24 @@
-buffer = ""
+buffer = ''
 
 // Keylistener for UUIDs/Barcodes
 document.addEventListener('keydown', logKey)
 
 function logKey (e) {
-    
-    if (e.key == 'Enter') {
-        // search for users with this ID
-        const users = get_users()
-        const foundUser = users.find(user => buffer.toUpperCase() === user.transponder.toUpperCase())
-        if (foundUser) {
-            // if we have a match, book a drink
-            console.log("Serial No. " + buffer + " belongs to " + foundUser.name)
-            add_drink(foundUser.name)
-        } else {
-            showAlert("Unbekannte Karte")
-        }
-        
-        // reset the buffer
-        buffer = ""
+  if (e.key == 'Enter') {
+    // search for users with this ID
+    const users = get_users()
+    const foundUser = users.find(user => buffer.toUpperCase() === user.transponder.toUpperCase())
+    if (foundUser) {
+      // if we have a match, book a drink
+      console.log('Serial No. ' + buffer + ' belongs to ' + foundUser.name)
+      add_drink(foundUser.name)
     } else {
-        buffer += e.key
+      showAlert('Unbekannte Karte')
     }
+
+    // reset the buffer
+    buffer = ''
+  } else {
+    buffer += e.key
+  }
 }
