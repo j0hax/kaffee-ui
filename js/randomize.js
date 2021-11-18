e8ff99757e76e6366b66fe8f410d998217a98791
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-11-18 12:38:14 +0100

    Remove unused file

--- js/randomize.js
+++
@@ -1,9 +0,0 @@
-// WARNING: demonstration code only!
-localStorage.users = ''
-
-for (let i = 0; i < 100; i++) {
-  const name = 'Name ' + i
-  const drinkCount = Math.floor(Math.random() * 100)
-  const balance = Math.floor(Math.random() * 5000)
-  add_user(name, drinkCount, balance)
-}
